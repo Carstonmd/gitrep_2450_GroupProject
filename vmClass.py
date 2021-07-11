71dbced343d759a5748bdd218c8ce2dcc23027d1
Andrew-L-Campbell <83891416+Andrew-L-Campbell@users.noreply.github.com>
2021-07-11 15:15:25 -0600

    created memory, created part of the dump method that displays the memory and structure

--- vmClass.py
+++ vmClass.py
@@ -1,7 +1,9 @@
 class virtualMachine:
 
     def __init__(self):
-        self.memory = [100]
+        self.memory = [[]]*100# memory is a list of lists declared here
+        for i in range(100):
+            self.memory[i] = [0,0,0,0,0]
         self.operand = 0
         self.exitCode = -99999
         self.opCode = 0
@@ -18,9 +20,32 @@ class virtualMachine:
     def LogError(message):
         print("Returns nothing")
     
+
     #Dump is a memory dump I think
-    def Dump():
-        print("Dump the memory!!")
+    def Dump(self):
+        print("\nREGISTERS:          ")
+        print("Accumulator:          ")# todo
+        print("InstrucctionCounter:  ")# todo
+        print("InstructionRegister:  ")# todo
+        print("OperationCode:        ")# todo
+        print("Operand:              ")# todo
+        #Below is getting the format of the array displayed
+        multiple = 0
+        counter = 10
+        print("\nMEMORY:")
+        print("    00     01     02     03     04     05     06     07     08     09",end="")
+        for index in self.memory:
+            if counter % 10 == 0:
+                print("\n"+str(multiple) + "0 ", end =" ")
+                multiple += 1
+                counter = 0
+            counter +=1    
+            for value in range(5):
+                word = str(index[value])
+                print(word, end ="")
+            print(" ",end=" ")
+
+
     #Calls the prompt to the console. This likely will be called on load.
     #this may return a string?
     def Prompt():
@@ -31,3 +56,15 @@ class virtualMachine:
     #this will validate input from users
     def validate(user_input):
         print("Validate here")
+
+#main method if we want it not in a seperate class
+def main():
+    vm = virtualMachine()
+    vm.Dump() # to access a certain memory location use vm.memory[i][j] 
+    
+    
+
+
+#running main
+if __name__ == "__main__":
+    main()
